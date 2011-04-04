f7efd17caf64c054eb725bd1cbe07e27b175f3a8
Chris Weyl <cweyl@alumni.drew.edu>
2011-04-03 18:45:47 -0700

    code/pod cleanup for Dist::Zilla

--- lib/MooseX/CascadeClearing.pm
+++ lib/MooseX/CascadeClearing.pm
@@ -1,5 +1,7 @@
 package MooseX::CascadeClearing;
 
+# ABSTRACT: Cascade clearer actions across attributes
+
 use warnings;
 use strict;
 
@@ -13,96 +15,6 @@ use Carp;
 #use Smart::Comments '###', '####';
 
 Moose::Exporter->setup_import_methods;
-
-=head1 NAME
-
-MooseX::CascadeClearing - Cascade clearer actions across attributes 
-
-=head1 VERSION
-
-Version 0.02
-
-=cut
-
-our $VERSION = '0.04';
-
-
-=head1 SYNOPSIS
-
-    use Moose;
-    use MooseX::CascadeClearing;
-
-    has master => (
-        is              => 'rw',
-        isa             => 'Str',
-        lazy_build      => 1,
-        is_clear_master => 1,
-    );
-
-    my @opts = (
-        is => 'ro', isa => 'Str', clear_master => 'master', lazy_build => 1,
-    );
-
-    has sub1 => @opts; 
-    has sub2 => @opts;
-    has sub3 => @opts;
-
-    sub _build_sub1 { shift->master . "1" }
-    sub _build_sub2 { shift->master . "2" }
-    sub _build_sub3 { shift->master . "3" }
-
-    sub some_sub {
-        # ... 
-
-        # clear master, sub[123] in one fell swoop
-        $self->clear_master;
-
-    }
-
-=head1 DESCRIPTION
-
-MooseX::CascadeClearing does the necessary metaclass fiddling to allow an
-clearing one attribute to be cascaded through to other attributes as well,
-calling their clearers.  
-
-The intended purpose of this is to assist in situations where the value of one
-attribute is derived from the value of another attribute -- say a situation
-where the secondary value is expensive to derive and is thus lazily built.  A
-change to the primary attribute's value would invalidate the secondary value
-and as such the secondary should be cleared.  While it could be argued that
-this is trivial to do manually for a few attributes, once we consider
-subclassing and adding in roles the ability to "auto-clear", as it were, is
-a valuable trait.  (Sorry, couldn't resist.)
-
-=head1 CAVEAT
-
-We don't yet trigger a cascade clear on a master attribute's value being set
-through a setter/accessor accessor.  This will likely be available as an
-option in the not-too-distant-future.
-
-=head1 ATTRIBUTE OPTIONS
-
-We install an attribute metaclass trait that provides two additional
-atttribute options, as well as wraps the generated clearer method for a
-designated "master" attribute.  By default, use'ing this module causes this
-trait to be installed for all attributes in the package.
-
-=over 4
-
-=item is_clear_master => (0|1)
-
-If set to 1, we wrap this attribute's clearer with a sub that looks for other
-attributes to clear.
-
-=item clear_master => Str
-
-Marks this attribute as one that should be cleared when the named attribute's
-clearer is called.  Note that no checking is done to ensure that the named
-master is actually an attribute in the class.
-
-=back
-
-=cut
 
 {
     package MooseX::CascadeClearing::Role::Meta::Attribute;
@@ -185,9 +97,86 @@ sub init_meta {
     return $for_class->meta;
 }
 
-=head1 AUTHOR
+1;
 
-Chris Weyl, C<< <cweyl at alumni.drew.edu> >>
+__END__
+
+=for Pod::Coverage init_meta
+
+=head1 SYNOPSIS
+
+    use Moose;
+    use MooseX::CascadeClearing;
+
+    has master => (
+        is              => 'rw',
+        isa             => 'Str',
+        lazy_build      => 1,
+        is_clear_master => 1,
+    );
+
+    my @opts = (
+        is => 'ro', isa => 'Str', clear_master => 'master', lazy_build => 1,
+    );
+
+    has sub1 => @opts; 
+    has sub2 => @opts;
+    has sub3 => @opts;
+
+    sub _build_sub1 { shift->master . "1" }
+    sub _build_sub2 { shift->master . "2" }
+    sub _build_sub3 { shift->master . "3" }
+
+    sub some_sub {
+        # ... 
+
+        # clear master, sub[123] in one fell swoop
+        $self->clear_master;
+
+    }
+
+=head1 DESCRIPTION
+
+MooseX::CascadeClearing does the necessary metaclass fiddling to allow an
+clearing one attribute to be cascaded through to other attributes as well,
+calling their clearers.  
+
+The intended purpose of this is to assist in situations where the value of one
+attribute is derived from the value of another attribute -- say a situation
+where the secondary value is expensive to derive and is thus lazily built.  A
+change to the primary attribute's value would invalidate the secondary value
+and as such the secondary should be cleared.  While it could be argued that
+this is trivial to do manually for a few attributes, once we consider
+subclassing and adding in roles the ability to "auto-clear", as it were, is
+a valuable trait.  (Sorry, couldn't resist.)
+
+=head1 CAVEAT
+
+We don't yet trigger a cascade clear on a master attribute's value being set
+through a setter/accessor accessor.  This will likely be available as an
+option in the not-too-distant-future.
+
+=head1 ATTRIBUTE OPTIONS
+
+We install an attribute metaclass trait that provides two additional
+atttribute options, as well as wraps the generated clearer method for a
+designated "master" attribute.  By default, use'ing this module causes this
+trait to be installed for all attributes in the package.
+
+=over 4
+
+=item is_clear_master => (0|1)
+
+If set to 1, we wrap this attribute's clearer with a sub that looks for other
+attributes to clear.
+
+=item clear_master => Str
+
+Marks this attribute as one that should be cleared when the named attribute's
+clearer is called.  Note that no checking is done to ensure that the named
+master is actually an attribute in the class.
+
+=back
 
 =head1 BUGS
 
@@ -236,28 +225,4 @@ fashion than I was previously.
 And of course the L<Moose> team, who have made my life significantly easier
 (and more fun!) since 0.17 :)
 
-=head1 COPYRIGHT & LICENSE
-
-Copyright (c) 2009, Chris Weyl C<< <cweyl@alumni.drew.edu> >>.
-
-This library is free software; you can redistribute it and/or modify it under
-the terms of the GNU Lesser General Public License as published by the Free 
-Software Foundation; either version 2.1 of the License, or (at your option) 
-any later version.
-
-This library is distributed in the hope that it will be useful, but WITHOUT 
-ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-OR A PARTICULAR PURPOSE.
-
-See the GNU Lesser General Public License for more details.  
-
-You should have received a copy of the GNU Lesser General Public License 
-along with this library; if not, write to the 
-
-    Free Software Foundation, Inc., 
-    59 Temple Place, Suite 330, 
-    Boston, MA  02111-1307 USA
-
 =cut
-
-1; # End of MooseX::CascadeClearing
